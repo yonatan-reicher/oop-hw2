1c6055f24b2659458a4480a9793db7b1ac025623
Yonatan Reicher <yony252525@gmail.com>
2024-02-13 16:44:45 +0200

    Starting work on HungryStudentImpl.java

---
+++ src/OOP/Solution/HungryStudentImpl.java
@@ -0,0 +1,44 @@
+package OOP.Solution;
+
+import OOP.Provided.HungryStudent;
+import OOP.Provided.Restaurant;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class HungryStudentImpl implements HungryStudent {
+    @Override
+    public HungryStudent favorite(Restaurant r) throws UnratedFavoriteRestaurantException {
+        return null;
+    }
+
+    @Override
+    public Collection<Restaurant> favorites() {
+        return null;
+    }
+
+    @Override
+    public HungryStudent addFriend(HungryStudent s) throws SameStudentException, ConnectionAlreadyExistsException {
+        return null;
+    }
+
+    @Override
+    public Set<HungryStudent> getFriends() {
+        return null;
+    }
+
+    @Override
+    public Collection<Restaurant> favoritesByRating(int rLimit) {
+        return null;
+    }
+
+    @Override
+    public Collection<Restaurant> favoritesByDist(int dLimit) {
+        return null;
+    }
+
+    @Override
+    public int compareTo(HungryStudent o) {
+        return 0;
+    }
+}
