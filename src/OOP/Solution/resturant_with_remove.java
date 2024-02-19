36ecc9aacc90c628a1ad5cf66cc6d5004ea95c31
ItayBenSasson <84470958+ItayBenSasson@users.noreply.github.com>
2024-02-19 17:06:30 +0200

    Add files via upload

---
+++ src/OOP/Solution/resturant_with_remove.java
@@ -0,0 +1,13 @@
+package OOP.Solution;
+
+import OOP.Provided.Restaurant;
+
+public class resturant_with_remove {
+    Restaurant new_Restaurant;
+    boolean remove;
+
+    public resturant_with_remove(Restaurant resturant) {
+        new_Restaurant =  resturant;
+        remove = false;
+    }
+}
